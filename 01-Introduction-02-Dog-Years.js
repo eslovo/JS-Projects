ed62d7b32dfb9b82b32f7f5ee816cd888dc4b453
eslovo <eslovo@gmail.com>
2021-01-12 12:00:02 +0300

    Create 01-Introduction-02-Dog-Years.js

---
+++ 01-Introduction-02-Dog-Years.js
@@ -0,0 +1,26 @@
+// my age
+const myAge =45;
+
+// dog age
+let earlyYears =2;
+
+// convert to dog-years
+earlyYears *=10.5;
+
+// later Yars
+let laterYears =myAge -2;
+
+// convert to dog-years
+laterYears *=4;
+
+// Testing
+//console.log(`Testing: earlyYears=${earlyYears} and laterYears =${laterYears}`);
+
+// Calculating my age in do years
+const myAgeInDogYears =earlyYears +laterYears;
+
+// set my name in lowercase
+let myName = 'Vitaly'.toLowerCase();
+
+// Result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
